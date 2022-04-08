575514c8d473e155d589fbc7e622d48312c0aea1
Bousaque <89078537+bousaque@users.noreply.github.com>
2022-04-08 15:17:18 +0200

    8 avril 2022 II

--- src/app/app.js
+++ src/app/app.js
@@ -1,14 +1,3 @@
-//AUTH
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-
-//DATABASE
-import { getDatabase, ref , get , child } from 'firebase/database';
-
-//Own
-import { PopupCandUI } from './components/popupCandUI';
-import { EspaceRecruteur } from "./features/espaceRecruteur";
-import { EspaceCandidat } from "./features/espaceCandidat";
-
 import { HeaderApp } from "./components/header";
 import { PwRecrUI } from "./components/pwRecrUI";
 import { FooterApp } from "./components/footer";
--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -18,11 +18,7 @@ export class FooterApp {
                 <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogo%20Geneveroule.png?alt=media&token=a66f7074-0388-405f-9a9f-f2a4bb140fa1" alt="Logo Geneveroule" />
             </a>
             <a href="https://clairbois.ch/">
-<<<<<<< HEAD
-                <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogo%20Clair-Bois.jpeg?alt=media&token=4d9da627-9c58-470a-9b0f-c0146f6b425d" alt="Logo Clair-Bois" />
-=======
                 <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogoClairBoisSq.svg?alt=media&token=c8a9c6f1-b001-4a5e-bb8d-b46ca0bd7e00" alt="Logo Clair-Bois" />
->>>>>>> cb7e91f9084f2263e55d60ea8977bcd0bfe9d6b2
             </a>
             <a href="https://www.022familles.ch/">
                 <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogo%20022%20Familles.jpeg?alt=media&token=311eb496-cb2d-4343-88e1-abc63b29f06e" alt="Logo 022Familles" />
@@ -33,20 +29,10 @@ export class FooterApp {
                 <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogo%20Espace%20Entreprise.png?alt=media&token=6cdfe7c0-6da7-45c5-9084-97945ab80ba4" alt="Logo Espace Entreprises" />
             </a>
             <a href="https://www.trajets.org/">
-<<<<<<< HEAD
-                <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogo%20Trajets%20Nom.png?alt=media&token=9895ed0c-4262-4dd0-9776-d4661aae6267" alt="Logo Trajets" />
-            </a>
-            <a href="https://www.pro-geneve.ch/">
-                <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogo%20PRO.png?alt=media&token=eb2bc533-70a8-4983-939a-a3c1c2bfa157" alt="Logo PRO" />
-            </a>
-            <a href="https://www.partage.ch/fr/">
-                <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2Flogo-partage.svg?alt=media&token=7183fb04-1122-4c43-9e46-6f189d4ea8fe" alt="Logo Partage" />
-=======
                 <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogoTrajetSq.svg?alt=media&token=2741bd8b-91a2-4653-b9e6-20dadc70ed9d" alt="Logo Trajets" />
             </a>
             <a href="https://www.pro-geneve.ch/">
                 <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogo%20PRO.png?alt=media&token=eb2bc533-70a8-4983-939a-a3c1c2bfa157" alt="Logo PRO" />
->>>>>>> cb7e91f9084f2263e55d60ea8977bcd0bfe9d6b2
             </a>
             <a href="https://www.partage.ch/fr/">
                 <img  class="logos_footer" src="https://firebasestorage.googleapis.com/v0/b/projet-nomades-1.appspot.com/o/general_media%2Flogos_footer%2FLogoPartageSq.svg?alt=media&token=470ef30d-9601-4193-9c4f-bf048e63fa01" alt="Logo Partage" />
